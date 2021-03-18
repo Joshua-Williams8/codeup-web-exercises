8dd4bc6e7cc87bbc3c34bc791246e532e6411308
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-03-18 15:29:19 -0500

    Just some testing Stuff

---
+++ js/JavascriptTest.js
@@ -0,0 +1,14 @@
+"use strict"
+var dude = " my guy";
+console.log("YOOOOO I'M IN HERE" + dude);
+//Single line comment
+/* block
+* multi
+* line
+* comment
+ */
+// var num = parseFloat(prompt("Gimme a number..."));
+//console.log("user enter: " + num + "That number plus 17 is: " + (num + 17))
+// var canIGo = confirm("Can I go to gym?");
+// console.log(canIGo)
+alert("You have a virus")
