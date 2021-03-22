9138b4273dd791f3d1b8f1a198677fa08df00f82
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-03-22 10:38:30 -0500

    Messing around with condition stuff

--- js/JavascriptTest.js
+++ js/JavascriptTest.js
@@ -34,9 +34,41 @@ var dude = " my guy";
 // processUserInput(greeting);
 
 
-console.log(me("Josh"))
-
-var Testiboi = function me(x) {
-  return "Something I did " + x
-}
+// console.log(Testiboi("Josh"))
+//
+// var Testiboi = function me(x) {
+//   return "Something I did " + x
+// }
 // above doesn't work cause variables aren't hoisted like a regular function declaration would be.
+//Below is a regular if else statement for looking at purposes for comparing.
+
+// var success = 2;
+// var message = (success === true) ? "Operation was successful." : (success === false) ? "Oops, something went wrong." : "This isn't a boolean?";
+//
+// var message;
+// if (success === true) {
+//   //You could just put success in here cause if it is true it'll do the operation but it's easier to read with the === true part
+//   message = "Operation was successful.";
+// } else if (success === false) {
+//   message = "Oops, something went wrong.(false)";
+// } else {
+//   message = "This was not a true or false thingy guy"}
+
+//Below is a an example of me using a break, instead of an if else or a ternary expression
+// var success = 2
+// switch(success) {
+//   case true :
+//     console.log("Operation was successful");
+//     break;
+//   case false :
+//     console.log("Operation was unsuccessful (false)");
+//     break;
+//     case false :
+//     console.log("Operation was unsuccessful (false)");
+//     break;
+//   default:
+//     console.log("This is not a true or false")
+//     break; //break is optional here.
+// }
+//The case part above, is like saying, if the input is === case
+//The : is what we do if the case part is true.
