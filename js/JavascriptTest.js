0dbed71b876d3068748f00e2c0421f2308886238
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-03-21 22:56:15 -0500

    Just messing around with function stuff

--- js/JavascriptTest.js
+++ js/JavascriptTest.js
@@ -1,6 +1,7 @@
-"use strict"
+"use strict" ;
+
 var dude = " my guy";
-console.log("YOOOOO I'M IN HERE" + dude);
+//console.log("YOOOOO I'M IN HERE" + dude);
 //Single line comment
 /* block
 * multi
@@ -11,4 +12,31 @@ console.log("YOOOOO I'M IN HERE" + dude);
 //console.log("user enter: " + num + "That number plus 17 is: " + (num + 17))
 // var canIGo = confirm("Can I go to gym?");
 // console.log(canIGo)
-alert("You have a virus")
+//alert("You have a virus");
+
+// var result = (function () {
+//   var name = "Barry";
+//   return name;
+// })();
+// var boi = function(y) {
+//   return y * y;
+// };
+
+// function greeting(name) {
+//   alert('Hello ' + name);
+// }
+//
+// function processUserInput(callback) {
+//   var name = prompt('Please enter your name.');
+//   callback(name);
+// }
+//
+// processUserInput(greeting);
+
+
+console.log(me("Josh"))
+
+var Testiboi = function me(x) {
+  return "Something I did " + x
+}
+// above doesn't work cause variables aren't hoisted like a regular function declaration would be.
