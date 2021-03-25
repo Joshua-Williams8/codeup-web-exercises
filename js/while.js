597516200fe8777ad85f92c2e1bae5e3a8fc7277
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-03-25 16:41:37 -0500

    loop stuff 1.1

--- js/while.js
+++ js/while.js
@@ -21,7 +21,46 @@ var test = 1
 //   //it will repeat lines above, until the condition is false.
 // }
 
-do{
-  console.log(test);
-  test = test * 2;
-} while (test <= 65536);
+//same as while, except it does the stuff first, then checks the condition, before looping through again.
+// do{
+//   console.log(test);
+//   test = test * 2;
+// } while (test <= 65536);
+
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+// Math.floor(Math.random() * 5) + 1;
+
+do {
+  //Below just gives a random number between 1 and 5, (it's the amount someone is buying)
+  var soldCones = Math.floor(Math.random() * 5) + 1;
+  //if statement for long all the amount I have left is greater than the amount sold do this.
+  if (allCones > soldCones) {
+    //redefining allCones to take away soldCones values each time.
+    allCones = allCones - soldCones;
+    //Logs to show what the cones values are.
+    console.log("I sold " + soldCones + " cones.");
+    console.log("I need to sell " + allCones + " more cones.");
+    //if the if statement above becomes false do this:
+    //I actually don't need the if... but I'm keeping it, cause it works as intended.
+  } else if (allCones <= soldCones){
+    //logs below to explain what's going on.
+    console.log("I cannot sell you " + soldCones + " cones, because I only have " + allCones + ".");
+    console.log("I'm almost out of cones, I can sell you " + allCones );
+    //I'm redefining soldCones to the last remaining AllCones.
+    soldCones = allCones;
+    //allCones is being defined with the remaining soldCones being taken from it's current values.
+    allCones = allCones - soldCones;
+    //log statements to show I'm selling the rest of my cones.
+    console.log("Here are my last " + soldCones + " cones.")
+    console.log("I have " + allCones + " left, I sold them all.")
+    //I wrote my code in a way where all the remaining values should make since, so
+    //Instead of doing allCones = allCones - allCones or 0, I wanted soldCones to reflect what
+    //actually at the time was being sold.
+  }
+} while (allCones > 0);
+// command shift / to uncomment a block comment. or command shift for a line comment
+
+
+
