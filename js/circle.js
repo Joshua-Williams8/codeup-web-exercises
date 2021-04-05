b974cf8c90b69abff7b9cc324db081ffdc7e2f68
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-04-05 09:54:35 -0500

    Math Object

---
+++ js/circle.js
@@ -0,0 +1,48 @@
+(function() {
+    "use strict";
+
+    // create a circle object
+    var circle = {
+        radius: 3,
+
+        getArea: function () {
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+
+            return (this.radius ** 2) * Math.PI; // TODO: return the proper value
+        },
+
+        logInfo: function (doRounding) {
+            // TODO: complete this method.
+          if (doRounding === true){
+
+
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(circle.getArea()));
+          } else {
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + circle.getArea());
+          }
+        },
+
+    };
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number" );
+    console.log(Math.round(circle.getArea()));
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+    // TODO: Change the radius of the circle to 5.
+    console.log("Changing radius to 5")
+    circle.radius = 5;
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+
+})();
---
+++ js/math-lecture.js
@@ -0,0 +1,18 @@
+"use strict"
+
+//math.random() by it self always return 1 or 0.
+//This is for 3, the 5 is the highest we go, the 1 is the lowest or first number...
+//var randomNumTest = Math.floor(Math.random() * 5) + 1;
+function getRandomInt(min,max){
+  return Math.floor(Math.random()* (max - min + 1) + min)
+}
+//Math.floor always rounds down
+//Math.round will round up or down
+//Math.ciel always rounds up
+//Math.pow(2,16) - will return 2 to the 16th power.
+//9.Math.sqrt() - returns the square root of 9
+//CONSTANTS...
+
+//Math.E - represents the base of natural logarithms,e,approximately is 2.718
+//Math.PI is PI like the full calculation.
+
