bad8f9699c7c6e1abf7c02dc2da06df2aa58bd3b
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-04-30 21:32:57 -0500

    jquery exercises

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,9 +1,23 @@
 // window.onload = function() {
 //   alert("Page loaded.");
 // };
-$(document).ready(function() {
-  alert("Page loaded.")
-});
+// $(document).ready(function() {
+//   alert("Page loaded.")
+// });
+// $(document).ready(function() {
+  // alert($("#alertMe").html())
+  // alert($("#unorderedList").html())
+//  You need the .html() with the parenthesis.
+// });
+// $(".codeup").css("border", "red 1px solid");
+$("li").css("font-size", "20px");
+$("h1,p,li").css("background-color","yellow");
+
+alert($("h1").html());
+
+
+
+
 
 
 
