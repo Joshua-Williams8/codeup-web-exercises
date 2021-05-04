f5a6a61cb66165cbd27a39b5c15ec8534c51860f
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-05-04 08:51:08 -0500

    Jquery

--- js/Functions_Drills.js
+++ js/Functions_Drills.js
@@ -586,3 +586,5 @@ function countVowels(string){
 }
 
 
+
+
--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -10,11 +10,24 @@
 //  You need the .html() with the parenthesis.
 // });
 // $(".codeup").css("border", "red 1px solid");
-$("li").css("font-size", "20px");
-$("h1,p,li").css("background-color","yellow");
-
-alert($("h1").html());
+// $("li").css("font-size", "20px");
+// $("h1,p,li").css("background-color","yellow");
+//
+// alert($("h1").html());
 //test of local storage stuff tomorrow
+
+$("h1").click(function(){
+  $(this).css("background-color","gold");
+});
+
+$("p").dblclick(function(){
+  $(this).css("font-size","18px");
+});
+$("li").hover(function(){
+  $(this).css("color","red");
+},function(){
+  $(this).css("color","black");
+});
 
 
 
