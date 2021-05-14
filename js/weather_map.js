51908e49855dab44d461604afd44e469d6a9e387
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-05-14 09:33:01 -0500

    es6

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,203 +1,193 @@
-$(document).ready(function(){
-var weatherData;
-var dateTimesStrings = [];
+$(document).ready(function () {
+  var weatherData;
+  var dateTimesStrings = [];
 // var todayDate
 // var today
-var removeMarkerArray = [];
-var marker;
+  var removeMarkerArray = [];
+  var marker;
 
-mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN
-var map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/dark-v10', // style URL
-  center: [-97.6709, 31.0689], // starting position [lng, lat]
-  zoom: 9 // starting zoom
-});
+  mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN
+  var map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/dark-v10', // style URL
+    center: [-97.6709, 31.0689], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+
+  }).addControl(new mapboxgl.NavigationControl());
 
 
-function weatherAjax(lng,lat) {
+  function weatherAjax(lng, lat) {
 
-  $.ajax(" https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lng + "&exclude=current,hourly,minutely&units=imperial&appid=" + OWM_TOKEN).done(function (resp) {
-    resp.daily.splice(5,3);
-    // console.log(resp)
-    weatherData = resp
-    // today = resp.daily[0]
+    $.ajax(" https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lng + "&exclude=current,hourly,minutely&units=imperial&appid=" + OWM_TOKEN).done(function (resp) {
+      resp.daily.splice(5, 3);
+      // console.log(resp)
+      weatherData = resp
+      // today = resp.daily[0]
 
-    // todayDate = new Date(today.dt * 1000)
-    // console.log(todayDate)
+      // todayDate = new Date(today.dt * 1000)
+      // console.log(todayDate)
 
-    //  dt * 1000? but uh oof.
+      //  dt * 1000? but uh oof.
 
-    function dayTimeHTML(day) {
-      var result = new Date(weatherData.daily[day].dt * 1000).toString();
-      // console.log(result);
-      var resultArray = result.split(" ");
-      // console.log(resultArray);
-      var resultObject = {
-        day: resultArray[0],
-        month: resultArray[1],
-        date: resultArray[2],
-        year: resultArray[3],
-      }
-      if (day === 0) {
-        resultObject.day = resultObject.day + " (Today)"
-      }
-      return resultObject;
-    }
-
-    function dateHTML(day) {
-      var result = new Date(weatherData.daily[day].dt * 1000).toString();
-      // console.log(result);
-      var resultArray = result.split(" ");
-      // console.log(resultArray);
-      var resultObject = {
-        day: resultArray[0],
-        month: resultArray[1],
-        date: resultArray[2],
-        year: resultArray[3],
+      function dayTimeHTML(day) {
+        var result = new Date(weatherData.daily[day].dt * 1000).toString();
+        // console.log(result);
+        var resultArray = result.split(" ");
+        // console.log(resultArray);
+        var resultObject = {
+          day: resultArray[0],
+          month: resultArray[1],
+          date: resultArray[2],
+          year: resultArray[3],
+        }
+        if (day === 0) {
+          resultObject.day = resultObject.day + " (Today)"
+        }
+        return resultObject;
       }
 
-      return resultObject;
-    }
+      function dateHTML(day) {
+        var result = new Date(weatherData.daily[day].dt * 1000).toString();
+        // console.log(result);
+        var resultArray = result.split(" ");
+        // console.log(resultArray);
+        var resultObject = {
+          day: resultArray[0],
+          month: resultArray[1],
+          date: resultArray[2],
+          year: resultArray[3],
+        }
+
+        return resultObject;
+      }
 
 
-    function tempHTML(day) {
-      return weatherData.daily[day].temp
-    }
+      function tempHTML(day) {
+        return weatherData.daily[day].temp
+      }
 
-    function weatherHTML(day) {
-      return weatherData.daily[day].weather[0]
-    }
+      function weatherHTML(day) {
+        return weatherData.daily[day].weather[0]
+      }
 
-    function rainHTML(day) {
-     var stuff = weatherData.daily[day].pop.toFixed(2) * 100;
-     stuff = stuff.toFixed(0);
-     return stuff + "%";
-    }
+      function rainHTML(day) {
+        var stuff = weatherData.daily[day].pop.toFixed(2) * 100;
+        stuff = stuff.toFixed(0);
+        return stuff + "%";
+      }
 
-    function humidHTML(day) {
-      return weatherData.daily[day].humidity + "%";
-    }
-
-
-    function capitalizeFirst(string){
-
-      var stringArray = string.toLowerCase().split(" ");
-      var resultArray = [];
-      stringArray.forEach(function(element,index){
-        var elementArray = element.split("");
-        // console.log(elementArray)
-        var firstLetter = elementArray[0].toUpperCase();
-        // console.log(firstLetter)
-        elementArray.splice(0,1,firstLetter);
-        // console.log(elementArray)
-        element = elementArray.join("")
-        resultArray.push(element);
-      });
-      return resultArray.join(" ");
-    }
+      function humidHTML(day) {
+        return weatherData.daily[day].humidity + "%";
+      }
 
 
+      function capitalizeFirst(string) {
+
+        var stringArray = string.toLowerCase().split(" ");
+        var resultArray = [];
+        stringArray.forEach(function (element, index) {
+          var elementArray = element.split("");
+          // console.log(elementArray)
+          var firstLetter = elementArray[0].toUpperCase();
+          // console.log(firstLetter)
+          elementArray.splice(0, 1, firstLetter);
+          // console.log(elementArray)
+          element = elementArray.join("")
+          resultArray.push(element);
+        });
+        return resultArray.join(" ");
+      }
 
 
+      function loadData() {
+        $("#days").html("");
+
+        weatherData.daily.forEach(function (day, index) {
+          // console.log(day,index)
+
+          $("#days").append(
+            "<div class=\"card weatherCard text-white bg-dark mx-2 grow animated fadeInDown\">\n" +
+            "    <img class='icons' src=\"http://openweathermap.org/img/wn/" + weatherData.daily[index].weather[0].icon + "@2x.png\n\" class=\"card-img-top\" alt=\"...\">\n" +
+            "    <div class=\"card-body cardInfo p-0\">\n" +
+            "      <h4 class=\"card-title pl-2\">" + dayTimeHTML(index).day + "</h4>\n" +
+            "      <ul class=\"list-group cardList m-0 py-0\">\n" +
+            "        <li class=\"list-group-item m-0 py-0\">" + dateHTML(index).year + "-" + dateHTML(index).month + "-" + dateHTML(index).date + "</li>\n" +
+
+            "        <li class=\"list-group-item m-0 py-0\">" + "<i class=\"fas fa-temperature-high\"></i>" + tempHTML(index).max + " - " + " <i class=\"fas fa-temperature-low\"></i>" + tempHTML(index).min + "</li>\n" +
+            "        <li class=\"list-group-item m-0 py-0\">" + capitalizeFirst(weatherHTML(index).description) + "</li>\n" +
+            "        <li class=\"list-group-item m-0 py-0\">" + "Chance of Rain: " + rainHTML(index) + "</li>\n" +
+            "        <li class=\"list-group-item m-0 py-0\">" + "Humidity: " + humidHTML(index) + "</li>\n" +
+            "      </ul>\n" +
+            // "      <a href=\"#\" class=\"btn btn-primary\">Go somewhere</a>\n" +
+            "    </div>\n" +
+            "  </div>"
+          )
+
+        });
 
 
-    function loadData() {
-      $("#days").html("");
+      }
 
-      weatherData.daily.forEach(function (day, index) {
-        // console.log(day,index)
+      loadData();
+      var newCenter = {
+        lng: lng,
+        lat: lat
+      };
 
-        $("#days").append(
-          "<div class=\"card weatherCard text-white bg-dark mx-2 grow animated fadeInDown\">\n" +
-          "    <img class='icons' src=\"http://openweathermap.org/img/wn/" + weatherData.daily[index].weather[0].icon + "@2x.png\n\" class=\"card-img-top\" alt=\"...\">\n" +
-          "    <div class=\"card-body cardInfo p-0\">\n" +
-          "      <h4 class=\"card-title pl-2\">" + dayTimeHTML(index).day + "</h4>\n" +
-          "      <ul class=\"list-group cardList m-0 py-0\">\n" +
-          "        <li class=\"list-group-item m-0 py-0\">" + dateHTML(index).year + "-" + dateHTML(index).month + "-" +  dateHTML(index).date + "</li>\n" +
+      if (marker === undefined) {
+      } else {
+        marker.remove();
+      }
+      $("#markerLocation").html("");
 
-          "        <li class=\"list-group-item m-0 py-0\">" + "<i class=\"fas fa-temperature-high\"></i>" + tempHTML(index).max + " - " + " <i class=\"fas fa-temperature-low\"></i>" + tempHTML(index).min + "</li>\n" +
-          "        <li class=\"list-group-item m-0 py-0\">" + capitalizeFirst(weatherHTML(index).description) + "</li>\n" +
-          "        <li class=\"list-group-item m-0 py-0\">" + "Chance of Rain: " + rainHTML(index) + "</li>\n" +
-          "        <li class=\"list-group-item m-0 py-0\">"+ "Humidity: " + humidHTML(index) +"</li>\n" +
-          "      </ul>\n" +
-          // "      <a href=\"#\" class=\"btn btn-primary\">Go somewhere</a>\n" +
-          "    </div>\n" +
-          "  </div>"
-        )
+      var testCode;
+      // console.log(reverseGeocode(newCenter,MAPBOX_ACCESS_TOKEN))
+      // reverseGeocode(newCenter,MAPBOX_ACCESS_TOKEN)
+      reverseGeocode(newCenter, MAPBOX_ACCESS_TOKEN).then(function (info) {
 
-      });
+        testCode = info.toString();
+        var resultArray = testCode.split(", ");
+        resultArray.shift();
+        testCode = resultArray.join(", ");
+        // console.log(testCode);
 
-
-    }
-
-    loadData();
-    var newCenter = {
-      lng: lng,
-      lat: lat
-    };
-
-    if (marker === undefined) {
-    } else {
-      marker.remove();
-    }
-    $("#markerLocation").html("");
-
-    var testCode;
-    // console.log(reverseGeocode(newCenter,MAPBOX_ACCESS_TOKEN))
-    // reverseGeocode(newCenter,MAPBOX_ACCESS_TOKEN)
-    reverseGeocode(newCenter,MAPBOX_ACCESS_TOKEN).then(function (info) {
-
-      testCode = info.toString();
-      var resultArray = testCode.split(", ");
-      resultArray.shift();
-      testCode = resultArray.join(", ");
-      // console.log(testCode);
-
-      // var markerInfo = markerNewLocation(newCenter)
-      $("#markerLocation").html(
-        testCode
-      );
+        // var markerInfo = markerNewLocation(newCenter)
+        $("#markerLocation").html(
+          testCode
+        );
 
       });
 
 
+      marker = new mapboxgl.Marker({
+        draggable: true,
+        color: "limegreen",
 
 
+      })
+        .setLngLat(newCenter)
+        .addTo(map);
 
-    marker = new mapboxgl.Marker({
-      draggable: true,
-      color: "limegreen",
+      marker.on("dragend", function () {
+        // console.log(marker.getLngLat());
+        var geoCenter = {
+          lng: marker.getLngLat().lng,
+          lat: marker.getLngLat().lat
+        }
+        map.flyTo({center: geoCenter, zoom: 9,});
+        weatherAjax(marker.getLngLat().lng, marker.getLngLat().lat);
+      });
 
-
-    })
-      .setLngLat(newCenter)
-      .addTo(map);
-
-    marker.on("dragend", function() {
-      // console.log(marker.getLngLat());
-      var geoCenter = {
-        lng: marker.getLngLat().lng,
-        lat: marker.getLngLat().lat
-      }
-      map.flyTo({center: geoCenter,zoom: 9,});
-      weatherAjax(marker.getLngLat().lng,marker.getLngLat().lat);
     });
+  }
 
-  });
-}
-var defaultCenter =
-  [-97.6709, 31.0689]
-;
-weatherAjax(defaultCenter[0],defaultCenter[1]);
-
+  var defaultCenter =
+    [-97.6709, 31.0689]
+  ;
+  weatherAjax(defaultCenter[0], defaultCenter[1]);
 
 
-removeMarkerArray.push(marker);
-
-
-
-
+  removeMarkerArray.push(marker);
 
 
   $("#markers").click(function () {
@@ -237,7 +227,7 @@ removeMarkerArray.push(marker);
         lat: info[1]
       };
 
-      map.flyTo({center: geoCenter,zoom: 9,});
+      map.flyTo({center: geoCenter, zoom: 9,});
 
       // var markerNew = new mapboxgl.Marker({
       //   draggable: false,
@@ -250,12 +240,9 @@ removeMarkerArray.push(marker);
 
       // removeMarkerArray.push(markerNew);
       console.log(geoCenter)
-      weatherAjax(geoCenter.lng,geoCenter.lat)
+      weatherAjax(geoCenter.lng, geoCenter.lat)
     });
   });
-
-
-
 
 
 // var removeMarkerArray = [];
